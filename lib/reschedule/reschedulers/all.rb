6f28ff94c0cd1c14b371496c4c409fc7c8f7354b
Tom Benner <tombenner@gmail.com>
2016-01-27 17:21:37 -0800

    Include more logging in dry run mode

--- lib/reschedule/reschedulers/all.rb
+++ lib/reschedule/reschedulers/all.rb
@@ -16,7 +16,6 @@ module Reschedule
         super
         replication_controllers = kubernetes_api.get_replication_controllers(namespace: options['namespace'])
         Reschedule.logger.debug "Found #{replication_controllers.length} replication controllers"
-        return if Reschedule.configuration.dry_run
 
         replication_controllers.select! do |replication_controller|
           image = replication_controller.spec['table'][:template]['spec']['containers'][0]['image']
@@ -33,12 +32,15 @@ module Reschedule
         replication_controller_name = replication_controller.metadata.name
         Reschedule.logger.debug "Rescheduling #{replication_controller_name}"
 
-        original_replicas = replication_controller.spec.replicas
-        replication_controller.spec.replicas = 0
-        kubernetes_api.update_replication_controller(replication_controller)
-        sleep 0.5
-        replication_controller.spec.replicas = original_replicas
-        kubernetes_api.update_replication_controller(replication_controller)
+        unless Reschedule.configuration.dry_run
+          original_replicas = replication_controller.spec.replicas
+          replication_controller.spec.replicas = 0
+          kubernetes_api.update_replication_controller(replication_controller)
+          sleep 0.5
+          replication_controller.spec.replicas = original_replicas
+          kubernetes_api.update_replication_controller(replication_controller)
+        end
+
         Reschedule.logger.debug "Rescheduled #{replication_controller_name}"
       end
     end
