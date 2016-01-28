d4c04035abda8a197d994d5a0f4ef260083d6a8f
Tom Benner <tombenner@gmail.com>
2016-01-27 17:57:58 -0800

    Support replication_controller_name_match in Reschedulers::All

--- lib/reschedule/reschedulers/all.rb
+++ lib/reschedule/reschedulers/all.rb
@@ -1,7 +1,7 @@
 module Reschedule
   module Reschedulers
     class All < Base
-      attr_reader :kubernetes_api
+      attr_reader :kubernetes_api, :replication_controller_name_regex
 
       self.defaults = {
         'namespace' => 'default'
@@ -10,6 +10,7 @@ module Reschedule
       def initialize(options={})
         super
         @kubernetes_api = Reschedule::Kubernetes::Api.new
+        @replication_controller_name_regex = options['replication_controller_name_match'].present? ? Regexp.new(options['replication_controller_name_match']) : nil
       end
 
       def run
@@ -19,8 +20,13 @@ module Reschedule
 
         replication_controllers.select! do |replication_controller|
           image = replication_controller.spec['table'][:template]['spec']['containers'][0]['image']
-          !image.include?('/reschedule')
+          next if image.include?('/reschedule')
+          next if replication_controller.spec.replicas == 0
+          next if replication_controller_name_regex && replication_controller.metadata.name !~ replication_controller_name_regex
+          true
         end
+
+        Reschedule.logger.debug "Found #{replication_controllers.length} replication controllers to reschedule"
         replication_controllers.each do |replication_controller|
           reschedule_replication_controller(replication_controller)
         end
